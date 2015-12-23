4cfa210215c4611c23ee16df14a6db369af233a4
jisaacstone <jisaacstone@gmail.com>
2015-12-23 10:30:44 -0800

    Updated README.md

--- mix.exs
+++ mix.exs
@@ -5,21 +5,23 @@ defmodule ArgumentParser.Mixfile do
     [app: :argument_parser,
      version: "0.0.2",
      elixir: "~> 1.1-dev",
+     package: package,
      name: "ArgumentParserEx",
      source_url: "https://github.com/jisaacstone/ex_argument_parser",
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     []
   end
 
+  defp package do
+   [licenses: ["Apache 2.0"],
+    links: %{"GitHub" => "https://github.com/jisaacstone/ex_argument_parser"}]
+  end
+
   def deps do
     [ {:dialyze, "~> 0.2", only: :dev},
-      {:earmark, "~> 0.1", only: :dev}, # markdown parser
-      {:ex_doc, "~> 0.11", only: :dev} ]
+      {:readme_md_doc, "~> 0.1", only: :dev} ]
   end
 end
