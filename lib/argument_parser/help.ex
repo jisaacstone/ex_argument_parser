d2e9d009cd8f67dc2f0a5066abfd643b821b1281
jisaacstone <jisaacstone@gmail.com>
2016-01-08 23:34:49 -0800

    help message formatting

--- lib/argument_parser/help.ex
+++ lib/argument_parser/help.ex
@@ -5,12 +5,12 @@ defmodule ArgumentParser.Help do
   def describe(%ArgumentParser{} = parser) do
     """
     #{parser.description}
-    #{print_body(parser)}
+    #{format_body(parser)}
     #{parser.epilog}
     """ |> String.strip()
   end
 
-  defp print_body(parser) do
+  defp format_body(parser) do
     flags = if parser.add_help do
       parser.flags ++ [[:help, alias: :h, action: :help,
                         help: "Display this message and exit"]]
@@ -19,65 +19,77 @@ defmodule ArgumentParser.Help do
     end
 
     [ "Usage:",
-      print_position_head(parser.positional),
+      format_position_head(parser.positional),
       "\n",
-      Enum.map(parser.positional, &print_positional/1),
+      format_args(:positional, parser.positional),
       "\nOptions:\n",
-      Enum.map(flags, &print_flag/1) ]
+      format_args(:flag, flags) ]
   end
 
-  defp print_position_head([]) do
+  defp format_position_head([]) do
     []
   end
-  defp print_position_head([[name | options] | rest]) do
-    mv = print_metavars(
+  defp format_position_head([[name | options] | rest]) do
+    mv = format_metavars(
       Keyword.get(options, :action, :store),
       Keyword.get(options, :metavar, Atom.to_string(name)))
-    [mv | print_position_head(rest)]
+    [mv | format_position_head(rest)]
   end
 
-  defp print_metavars(:store, mv) do
+  defp format_metavars(:store, mv) do
     [?\ , mv]
   end
-  defp print_metavars(t, mv) when is_tuple(t) do
-    print_metavars(elem(t, 1), mv)
+  defp format_metavars(t, mv) when is_tuple(t) do
+    format_metavars(elem(t, 1), mv)
   end
-  defp print_metavars(n, mv) when is_number(n) do
+  defp format_metavars(n, mv) when is_number(n) do
     (for _ <- 1..n, do: "#{ mv}") |> Enum.join
   end
-  defp print_metavars(:*, mv) do
+  defp format_metavars(:*, mv) do
     " [#{mv} ...]"
   end
-  defp print_metavars(:+, mv) do
+  defp format_metavars(:+, mv) do
     " #{mv} [#{mv} ...]"
   end
-  defp print_metavars(action, mv)
+  defp format_metavars(action, mv)
   when action in [:'?', :store_true, :store_false, :store_const] do
     " [#{mv}]"
   end
-  defp print_metavars(_, _) do
+  defp format_metavars(_, _) do
     ""
   end
 
-  defp print_positional([name | options]) do
-    ["\t#{name}", print_arg_help(options)]
+  defp longest_name(args) do
+    Stream.map(args, &Kernel.hd/1) |>
+      Stream.map(&Atom.to_string/1) |>
+      Stream.map(&String.length/1) |>
+      Enum.max()
   end
 
-  defp print_flag([name | options]) do
-    [ "\t--#{name}",
-      (if alias = options[:alias], do: " -#{alias}", else: ""),
-      print_arg_help(options) ]
+  defp format_args(_, []), do: ""
+  defp format_args(type, args) do
+    Enum.map(args, &format_arg(type, &1, longest_name(args)))
   end
 
-  defp print_arg_help(options) do
+  defp format_arg(:positional, [name | options], name_len) do
+    [:io_lib.format('  ~-#{name_len}s', [name]), format_arg_help(options)]
+  end
+  defp format_arg(:flag, [name | options], name_len) do
+    [ "  ",
+      (if alias = options[:alias], do: "-#{alias} ", else: ""),
+      :io_lib.format('--~-#{name_len}s', [name]),
+      format_arg_help(options)]
+  end
+
+  defp format_arg_help(options) do
     [ if choices = Keyword.get(options, :choices) do
-        " one of #{inspect(choices)}"
+        "  one of #{inspect(choices)}"
       else "" end,
       if default = Keyword.get(options, :default) do
-        " default: #{default}"
+        "  default: #{default}"
       else "" end,
       if help = Keyword.get(options, :help) do
-        " #{help}"
+        "  #{help}"
       else "" end,
       "\n" ]
   end
--- test/argument_parser_help_test.exs
+++ test/argument_parser_help_test.exs
@@ -2,21 +2,28 @@ defmodule ArgumentParser.Help.Test do
   alias ArgumentParser, as: AP
   use ExUnit.Case, async: :true
 
+  defmacrop contains(msg, string) do
+    quote do
+      assert(String.contains?(unquote(msg), unquote(string)),
+             ~s("#{unquote(string)}" not found in message:\n#{unquote(msg)}))
+    end
+  end
+
   test "description" do
     msg = AP.new(description: "Kirkland Singature") |> AP.print_help()
-    assert String.contains?(msg, "Kirkland Singature"), msg
+    contains(msg, "Kirkland Singature")
   end
 
   test "epilog" do
     msg = AP.new(epilog: "Mixed Nuts") |> AP.print_help()
-    assert String.contains?(msg, "Mixed Nuts"), msg
+    contains(msg, "Mixed Nuts")
   end
 
   test "arg help" do
     msg = AP.new() |>
       AP.add_arg(:foo, help: "do a foo") |>
       AP.print_help()
-    assert String.contains?("#{msg}", "do a foo"), msg
+    contains("#{msg}", "do a foo")
   end
 
   test "position head" do
@@ -25,7 +32,7 @@ defmodule ArgumentParser.Help.Test do
       AP.add_arg(:bar, action: :store_true) |>
       AP.add_arg(:buzz, action: {:store, :*}) |>
       AP.print_help()
-    assert String.contains?(msg, "Usage: foo [bar] [buzz ...]\n"), msg
+    contains(msg, "Usage: foo [bar] [buzz ...]\n")
   end
 
   test "position head macro" do
@@ -40,6 +47,15 @@ defmodule ArgumentParser.Help.Test do
       end
     end
     msg = PHM.help()
-    assert String.contains?(msg, "Usage: foo [bar] buzz [buzz ...]\n"), msg
+    contains(msg, "Usage: foo [bar] buzz [buzz ...]\n")
+  end
+
+  test "arg messages" do
+    msg = AP.new(add_help: :false) |>
+      AP.add_arg(:foo, help: "fubar") |>
+      AP.add_flag(:bar, alias: :b) |>
+      AP.print_help()
+    contains(msg, "  foo  fubar\n")
+    contains(msg, "  -b --bar")
   end
 end
